ab041ad101bf96a0fa8c20598b1672fc7da5eec2
Morgan Askins <maskins@ucdavis.edu>
2020-10-20 16:17:47 -0700

    Energy resolution namespace

--- src/src/EnergyResponse.cc
+++ src/src/EnergyResponse.cc
@@ -5,11 +5,11 @@ namespace frp
 {
   EnergyResponse::EnergyResponse(double energy, double mean_hits, double resolution )
   {
-    conversion     = mean_hits / energy;
-    minimum_energy = mean_hits - 20*resolution;
-    maximum_energy = mean_hits + 20*resolution;
-    mean           = mean_hits;
-    resolution     = resolution;
+    conversion           = mean_hits / energy;
+    minimum_energy       = mean_hits - 20*resolution;
+    maximum_energy       = mean_hits + 20*resolution;
+    mean                 = mean_hits;
+    this->resolution     = resolution;
     this->function = std::unique_ptr<TF1> (
       new TF1("ERFunction", "1/sqrt(2*pi*[0]^2)*exp(-(x-[1])^2/2/[0]^2)", minimum_energy, maximum_energy) );
     this->function->SetParameter(0, resolution);
@@ -20,7 +20,9 @@ namespace frp
   {
     // Check if event is out of bounds
     if( (energy <=0.0) || (resolution <= 0.001) || (mean == 0.0) )
+    {
       return 0;
+    }
     // We choose to assume that the nhit response is locally linear.
     this->function->SetParameter(1, energy*conversion);
     double testenergy = this->function->GetRandom();
